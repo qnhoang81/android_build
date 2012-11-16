221af0319eb624497bb5aa71b21b045558225bd5
qnhoang81 <qnhoang81@yahoo.com>
2012-11-16 10:29:25 -0700

    Disable ODEX
    Change-Id: I47be6f57ce358943c0550c7a3171a68a3840d927

--- core/main.mk
+++ core/main.mk
@@ -278,17 +278,6 @@ ifneq (,$(user_variant))
     enable_target_debugging :=
   endif
 
-  # Turn on Dalvik preoptimization for user builds, but only if not
-  # explicitly disabled and the build is running on Linux (since host
-  # Dalvik isn't built for non-Linux hosts).
-  ifneq (true,$(DISABLE_DEXPREOPT))
-    ifeq ($(user_variant),user)
-      ifeq ($(HOST_OS),linux)
-        WITH_DEXPREOPT := true
-      endif
-    endif
-  endif
-
   # Disallow mock locations by default for user builds
   ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=0
 
